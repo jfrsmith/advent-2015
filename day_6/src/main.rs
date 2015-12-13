c661aee215e4e2199afdc9a6321b6bb8de00cd95
Jack Smith <jfrsmith@me.com>
2015-12-13 19:24:36 +0000

    day_6 and day_7 complete@

--- day_6/src/main.rs
+++ day_6/src/main.rs
@@ -1,5 +1,6 @@
 use std::io::{BufReader,BufRead};
 use std::fs::File;
+use std::cmp;
 
 enum Command {
     Toggle,
@@ -34,27 +35,34 @@ fn parse_instruction(raw_instruction: &str) -> Instruction {
 					command: cmd  }
 }
 
-fn modify_lights(light_map: &[bool], do_instruction: Instruction) -> () {
-    // add code here
-}
-
-fn main() {
-	let mut lights: [bool; 1000*1000] = [false; 1000 * 1000];
-
-    let file = File::open("input/input.txt").unwrap();
-    for line in BufReader::new(file).lines() {
-    	modify_lights(&lights, parse_instruction(&line.unwrap()));
-    }
-
-    let mut num_on_lights = 0;
-
-    for x in 0..1000 {
-        for y in 0..1000 {
-            if lights[(x * 1000) + y] {
-                num_on_lights += 1;
+fn modify_lights(light_map: &mut Vec<i32>, do_instruction: Instruction) -> () {
+    for x in do_instruction.start.0..(do_instruction.end.0+1){
+        for y in do_instruction.start.1..(do_instruction.end.1+1) {
+            let index: usize = ((x*1000)+y) as usize;
+            match do_instruction.command {
+                Command::Toggle => light_map[index] += 2,
+                Command::TurnOn => light_map[index] += 1,
+                Command::TurnOff => light_map[index] = cmp::max(light_map[index] - 1, 0)
             }
         }
     }
+}
 
-    println!("num_on_lights = {}", num_on_lights);
+fn main() {
+    let mut lights = vec![0; 1000*1000];
+
+    let file = File::open("input/input.txt").unwrap();
+    for line in BufReader::new(file).lines() {
+    	modify_lights(&mut lights, parse_instruction(&line.unwrap()));
+    }
+
+    let mut total_brightness = 0;
+
+    for x in 0..1000 {
+        for y in 0..1000 {
+            total_brightness += lights[(x * 1000) + y];
+        }
+    }
+
+    println!("total_brightness = {}", total_brightness);
 }
