ecc872a9752e9ac98e857d05cafd1c86f6be911f
Jack Smith <jack.smith@splashdamage.local>
2015-12-14 17:22:06 +0000

    day_8 complete

--- day_8/src/main.rs
+++ day_8/src/main.rs
@@ -3,34 +3,27 @@ use std::fs::File;
 
 fn main() {
 	let mut total_code_chars = 0;
-	let mut total_string_chars = 0;
+	let mut total_encoded_chars = 0;
     let file = File::open("input/input.txt").unwrap();
     for line in BufReader::new(file).lines() {
-    	let s = line.unwrap();
+    	let mut s = line.unwrap().to_string();
 
-    	let mut curr_code_chars = s.len();
-    	let mut curr_string_chars = s.len() - 2;
+    	let curr_code_chars = s.len();
+        let mut curr_encoded_chars = s.len() + 2;
 
-    	let mut vec_chars = s.chars().collect();
+        for c in s.chars() {
+            match c {
+                '\\' => curr_encoded_chars += 1,
+                '\"' => curr_encoded_chars += 1,
+                _ => {}
+            }
+        }
 
-    	for (i, c) in vec_chars.iter().enumerate() {
-    	    if c == '\\' {
-    	        if i + 1 < vec_chars.num() {
-    	            match vec_chars[i+1] {
-    	                'x' => curr_string_chars -= 3,
-    	                '\"' => curr_string_chars -= 1,
-    	                '\\' => curr_string_chars -= 1,
-    	                _ => {}
-    	            }
-    	        }
-    	    }
-    	}
-
- 		println!("{} -> num_chars = {} num_str = {}", s, curr_code_chars, curr_string_chars);
+        println!("{} -> num_chars = {} num_str = {}", s, curr_code_chars, curr_encoded_chars);
 
     	total_code_chars += curr_code_chars;
-    	total_string_chars += curr_string_chars;
+    	total_encoded_chars += curr_encoded_chars;
     }
 
-    println!("total_code_chars = {} total_string_chars = {} total = {}", total_code_chars, total_string_chars, total_code_chars - total_string_chars);
+    println!("total_encoded_chars = {} total_code_chars = {} total = {}", total_encoded_chars, total_code_chars, total_encoded_chars - total_code_chars);
 }
